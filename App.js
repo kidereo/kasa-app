f433f8fce62a622eaf1151913bc77faf235fa30a
kidereo <kirill.derevenski@gmail.com>
2023-02-11 18:12:42 +0100

    Minor file reorganisation.

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Initial installation</Text>
-            <StatusBar style="auto"/>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Initial installation</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    alignContent: "center",
+  },
 });
